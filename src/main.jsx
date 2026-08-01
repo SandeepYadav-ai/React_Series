3562ef9ea6facbe04c752bbd3a95c4b93d72fea2
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-08-01 18:23:10 +0530

    fix head error

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
->>>>>>> 8b82440a5f888235c344090b93c69798df9034b2
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-// behind he seen how ot work variale to evalute expression (reactElement)
-// inside 
-//let reactElement = React.createElement("a",{href: 'https://google.cpm'},'visit') 
-=======
->>>>>>> 8b82440a5f888235c344090b93c69798df9034b2
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-<<<<<<< HEAD
-    {/* <reactElement /> */}
   </StrictMode>
-=======
-  </StrictMode>,
->>>>>>> 8b82440a5f888235c344090b93c69798df9034b2
 )
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-=======
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
->>>>>>> 8b82440a5f888235c344090b93c69798df9034b2
 })

4e9a9baadcec31733345929744b7b0af0a13d84f
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-09-07 19:05:22 +0530

    cleanUp function in useEffect

--- src/components/hooks/useEffect/CleanUpEffect.jsx
+++ src/components/hooks/useEffect/CleanUpEffect.jsx
@@ -4,15 +4,15 @@ const CleanUpEffect = ()=> {
     const [count, setCount] = useState(134);
     useEffect(()=> {
         const data = setInterval(()=> {
-            setCount((prev)=> prev + 1)
-        }, 1000)
-        return()=> {
-            clearInterval(data);
-        };
-    },[])
+            setCount((prev)=> prev + 1);
+        }, 1000);
+        return()=> clearInterval(data);
+    }, []);
+    console.log(count);
+    
     return(
         <div>
-            <h1>Live: The number of children born in KEM Hospital</h1>
+            <h1>Live: The number of children born in All Hospital</h1>
             <p><b>count:-</b> {count} </p>
         </div>
     );

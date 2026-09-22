deaf367e1497a5cfae56b5f0d60f30ae212d7b50
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-09-22 20:18:50 +0530

    useRef hook

--- src/components/hooks/useRefs/UseRef.jsx
+++ src/components/hooks/useRefs/UseRef.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 export const UseRef = function() {
+    const username = document.getElementById('username');
+    
+    
+    
+    //const [username, setUsername] = useState("");
+    //const [password, setPassword] = useState("");
+    const eventHandle = (e)=> {
+        e.preventDefault();
+        console.log(username.target.value);
+    };
+    //console.log(eventHandle);
+    
     return(
         <div>
-            <h1>hello useref</h1>
+            <form onSubmit={eventHandle}>
+                <label htmlFor="">username</label>
+                <input 
+                    type="text"
+                    id="username"
+                    //placeholder="enter usename"
+                    //value={username}
+                    //onChange={(e)=> setUsername(e.target.value)} 
+                /><br/>
+                <label htmlFor="">password</label>
+                <input 
+                    type="text"
+                    id="password"
+                    //placeholder="enter password"
+                    //value={password}
+                    //onChange={(e)=> setPassword(e.target.value)} 
+                />
+                <button>submit</button>
+            </form>
+            
         </div>
     );
 };

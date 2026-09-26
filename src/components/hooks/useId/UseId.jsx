28f11615144d7a455c24e9043f04846de52b6c1a
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-09-26 20:15:58 +0530

    useId hook

--- src/components/hooks/useId/UseId.jsx
+++ src/components/hooks/useId/UseId.jsx
@@ -1,24 +1,47 @@
 import { useId } from "react";
 
 export const UseId = function() {
-    const usernameId = useId();
-    const emailId = useId();
-    const passwordId = useId();
+    // const usernameId = useId();
+    // const emailId = useId();
+    // const passwordId = useId();
 
+
+    // return(
+    //     <form action="">
+    //         <div>
+    //             <label htmlFor={usernameId}>Username:</label>
+    //             <input type="text" id={usernameId} name="name" />
+    //         </div>
+    //         <div>
+    //             <label htmlFor={emailId}>Email:</label>
+    //             <input type="email" id={emailId} name="email" />
+    //         </div>
+    //         <div>
+    //             <label htmlFor={passwordId}>Password:</label>
+    //             <input type="password" id={passwordId} name="password" />
+    //         </div>
+    //         <button className="counter">Submit</button>
+    //     </form>
+    // );
+
+    //this lets you avoids calling useId for every single element thats needs unique Id
+    //using concate methord
+
+    const id = useId();
 
     return(
         <form action="">
             <div>
-                <label htmlFor={usernameId}>Username:</label>
-                <input type="text" id={usernameId} name="name" />
+                <label htmlFor={id + "usernameId"}>Username:</label>
+                <input type="text" id={id + "usernameId"} name="name" />
             </div>
             <div>
-                <label htmlFor={emailId}>Email:</label>
-                <input type="email" id={emailId} name="email" />
+                <label htmlFor={id+ "emailId"}>Email:</label>
+                <input type="email" id={id + "emailId"} name="email" />
             </div>
             <div>
-                <label htmlFor={passwordId}>Password:</label>
-                <input type="password" id={passwordId} name="password" />
+                <label htmlFor={id + "passwordId"}>Password:</label>
+                <input type="password" id={id + "passwordId"} name="password" />
             </div>
             <button className="counter">Submit</button>
         </form>
